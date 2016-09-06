806673cf94af52ff2012478e336272dc1eb67986
Donnie <Donnie@DESKTOP-A0TIJCR>
2016-09-06 10:28:58 +0200

    Add project files.

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01_Calculator
+{
+    internal class Calculator
+    {
+        internal static int Add(int v1, int v2)
+        {
+           return v1 + v2;
+        }
+
+        internal static int Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        internal static int Sum(int[] numbers)
+        {
+            numbers = new int[2];
+            return numbers[0] + numbers[1];
+        }
+    }
+}
